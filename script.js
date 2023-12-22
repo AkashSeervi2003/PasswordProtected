f646cab1ea9c86ae915d12acb74c2ac91acab454
AkashSeervi2003 <akashseervi402@gmail.com>
2023-12-22 16:03:22 +0530

    SecondCommit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function checkPassword() {
 
     // Replace 'your_password' with the actual password you want to use
     if (password === "AkashSuman") {
-        window.location.href = "https://akashseervi2003.github.io/AkashSeervi/";
+        window.location.href = "https://6585652d38826ab17b269c59--wondrous-cactus-059199.netlify.app/";
     } else {
         alert("Incorrect password. Please try again.");
     }
