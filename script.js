5737ac4646e6fa79cc191aae495acc5b53e0dbaf
AkashSeervi2003 <akashseervi402@gmail.com>
2023-12-22 12:34:17 +0530

    FirstCommit

---
+++ script.js
@@ -0,0 +1,11 @@
+function checkPassword() {
+    var password = document.getElementById("password").value;
+
+    // Replace 'your_password' with the actual password you want to use
+    if (password === "AkashSuman") {
+        window.location.href = "https://akashseervi2003.github.io/AkashSeervi/";
+    } else {
+        alert("Incorrect password. Please try again.");
+    }
+}
+
